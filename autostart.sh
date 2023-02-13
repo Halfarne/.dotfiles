6d49f45a181af0794b4e0bd07c7074ef1902330e
Halfarne <adam.mihula@seznam.cz>
2023-02-13 18:18:02 +0100

    autostart programs are now in 1 file for dwl and hyprland

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+~/.config/plocha/plocha.sh
+
+dunst
+
+"gsettings set org.gnome.desktop.interface gtk-theme 'Everforest-Dark-B'"
+"gsettings set org.gnome.desktop.interface icon-theme 'ePapirus'"
+"gsettings set org.gnome.desktop.interface font-name 'mononoki Nerd Font 11'"
