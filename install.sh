6eb219a9c967737eddcf74838e49634a86c40439
Halfarne <adam.mihula@seznam.cz>
2023-05-18 19:20:24 +0200

    Adding install dotfile script

---
+++ install.sh
@@ -0,0 +1,66 @@
+!/usr/bin/env bash
+
+ln=0
+cp=0
+
+echo "Be sure to be in .dotfiles directory and don't forget to change home folder ;)."
+
+read -p "Press enter to continue"
+
+read -p "Link FIles? (Y/N): " confirm && [[ $confirmln == [yY] || $confirmln == [yY][eE][sS] ]] || echo Proceeding to copying files ...
+
+if [[ $confirmln == [yY] ]]
+  then
+
+    ln -s /home/halfarne/.dotfiles/.config/hypr/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/dunst/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/kitty/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/gtk-3.0/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/plocha/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/spotifyd/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/spotify-tui/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/zathura/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/nvim/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/rofi/ /home/halfarne/.config/
+    ln -s /home/halfarne/.dotfiles/.config/autostart.sh /home/halfarne/.config/
+
+    ln -s /home/halfarne/.dotfiles/.themes/ /home/halfarne/
+
+    ln -s /home/halfarne/.dotfiles/.bashrc /home/halfarne/
+    ln -s /home/halfarne/.dotfiles/.bash_profile /home/halfarne/
+
+    ln -s /home/halfarne/.dotfiles/.hyprinitrc /home/halfarne/
+  fi
+
+read -p "Copy Files?(Y/N): " confirm && [[ $confirmcp == [yY] || $confirmcp == [yY][eE][sS] ]]
+
+if [[ $confitmcp == [yY] ]]
+  then
+
+    cp /home/halfarne/.dotfiles/.config/hypr/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/dunst/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/kitty/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/gtk-3.0/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/plocha/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/spotifyd/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/spotify-tui/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/zathura/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/nvim/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/rofi/ /home/halfarne/.config/
+    cp /home/halfarne/.dotfiles/.config/autostart.sh /home/halfarne/.config/
+
+    cp /home/halfarne/.dotfiles/.themes/ /home/halfarne/
+
+    cp /home/halfarne/.dotfiles/.bashrc /home/halfarne/
+    cp /home/halfarne/.dotfiles/.bash_profile /home/halfarne/
+
+    cp /home/halfarne/.dotfiles/.hyprinitrc /home/halfarne/
+  fi
+
+if [[ $confirmln != [yY] && $confirmcp != [yY] ]]
+  then
+
+    echo No option selected
+  fi
+
+read -p "Exiting program..." && exit 1
