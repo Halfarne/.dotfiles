8d3cb6960aabe9cf0544b8f03359a421ddfa190a
Halfarne <adam.mihula@seznam.cz>
2023-07-17 21:56:52 +0200

    addidng mpd to configuration

--- conf.nix
+++ conf.nix
@@ -322,6 +322,19 @@
     password = "halfarne";
   };
 
+  services.mpd = {
+  enable = true;
+  musicDirectory = "/mnt/1TB-hdd/Hudba";
+  extraConfig = ''
+    # must specify one or more outputs in order to play audio!
+    # (e.g. ALSA, PulseAudio, PipeWire), see next sections
+  '';
+
+  # Optional:
+  network.listenAddress = "any"; # if you want to allow non-localhost connections
+  startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+};
+
 #--not working:
   # Pipewore
 #  hardware.pulseaudio.enable = false;
